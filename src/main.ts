ac63840fe3147bdf337ea636980294b4bcc4043b
pedja98 <predragmirkovic1998@gmail.com>
2024-09-29 19:35:28 +0200

    add cors and env.sample

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api/v1')
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
   await app.listen(4000)
 }
 bootstrap()
