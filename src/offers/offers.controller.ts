530f05b963fdee05f84b68274806aeba5053a427
pedja98 <predragmirkovic1998@gmail.com>
2025-06-18 17:07:04 +0200

    fix bug

--- src/offers/offers.controller.ts
+++ src/offers/offers.controller.ts
@@ -9,8 +9,8 @@ export class OffersController {
   @Post()
   async proxyRequest(@Req() req: Request, @Res() res: Response) {
     try {
-      const data = await this.offersService.createOffer(req)
-      res.status(200).send(data)
+      const { data, status } = await this.offersService.createOffer(req)
+      res.status(status).send(data)
     } catch (error) {
       res.status(error.getStatus?.() || 500).send(error.message || 'Internal server error')
     }
