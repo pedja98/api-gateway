23a6743d5518ed0f222b10ca3c713e864b950c10
pedja98 <predragmirkovic1998@gmail.com>
2025-06-24 23:07:54 +0200

    fix actions

--- src/modules/offers/consts/offers.ts
+++ src/modules/offers/consts/offers.ts
@@ -1,14 +1,33 @@
 import { OfferStatus } from '../enums/offer-status.enum'
 
 export const AvailableOfferStatuses = {
-  [OfferStatus.DRAFT]: [OfferStatus.L1_PENDING, OfferStatus.L2_PENDING, OfferStatus.SALESMEN_CLOSED],
-  [OfferStatus.L1_PENDING]: [OfferStatus.L1_REJECTED, OfferStatus.OFFER_APPROVED, OfferStatus.SALESMEN_CLOSED],
-  [OfferStatus.L2_PENDING]: [OfferStatus.L2_REJECTED, OfferStatus.OFFER_APPROVED, OfferStatus.SALESMEN_CLOSED],
+  [OfferStatus.DRAFT]: [
+    OfferStatus.L1_PENDING,
+    OfferStatus.L2_PENDING,
+    OfferStatus.SALESMEN_CLOSED,
+    OfferStatus.SALESMEN_CLOSED,
+  ],
+  [OfferStatus.L1_PENDING]: [
+    OfferStatus.L1_REJECTED,
+    OfferStatus.OFFER_APPROVED,
+    OfferStatus.SALESMEN_CLOSED,
+    OfferStatus.SALESMEN_CLOSED,
+  ],
+  [OfferStatus.L2_PENDING]: [
+    OfferStatus.L2_REJECTED,
+    OfferStatus.OFFER_APPROVED,
+    OfferStatus.SALESMEN_CLOSED,
+    OfferStatus.SALESMEN_CLOSED,
+  ],
   [OfferStatus.SALESMEN_CLOSED]: [],
   [OfferStatus.CLOSED_BY_SYSTEM]: [],
   [OfferStatus.L1_REJECTED]: [],
   [OfferStatus.L2_REJECTED]: [],
-  [OfferStatus.OFFER_APPROVED]: [OfferStatus.CUSTOMER_ACCEPTED, OfferStatus.SALESMEN_CLOSED],
-  [OfferStatus.CUSTOMER_ACCEPTED]: [OfferStatus.CLOSED_BY_SYSTEM, OfferStatus.CONCLUDED],
+  [OfferStatus.OFFER_APPROVED]: [
+    OfferStatus.CUSTOMER_ACCEPTED,
+    OfferStatus.SALESMEN_CLOSED,
+    OfferStatus.SALESMEN_CLOSED,
+  ],
+  [OfferStatus.CUSTOMER_ACCEPTED]: [OfferStatus.CLOSED_BY_SYSTEM, OfferStatus.CONCLUDED, OfferStatus.SALESMEN_CLOSED],
   [OfferStatus.CONCLUDED]: [],
 }
